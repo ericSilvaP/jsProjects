9bc53621df013b409ee9e20e4c0b88bd922b2027
Eric Silva <ericsilvap13@gmail.com>
2025-02-14 14:53:14 -0300

    Criacao de contador automatico

--- contadorAutomatico/script.js
+++ contadorAutomatico/script.js
@@ -1,7 +1,47 @@
 
 const buttons = document.querySelectorAll(".btt");
+const counter = document.getElementsByClassName("number")[0];
+let actualNumber, id;
+
 buttons.forEach(element => {
     element.addEventListener("click", (e) => {
-        console.log(e.currentTarget.classList)
+        const styles = e.currentTarget.classList;
+        if (styles.contains("reverse")) {
+            if (id) {
+                clearInterval(id);
+            }
+            actualNumber = counter.innerText;
+            id = setInterval(() => {
+                actualNumber--;
+                counter.innerHTML = actualNumber;
+                verifyNumber();
+            }, 1000);
+        } else if (styles.contains("stop")) {
+            if (id) {
+                clearInterval(id);
+            }
+        } else {
+            if (id) {
+                clearInterval(id);
+            }
+            actualNumber = counter.innerText;
+            id = setInterval(() => {
+                actualNumber++
+                counter.innerHTML = actualNumber;
+                verifyNumber();
+            }, 1000);
+        }
+
+        function verifyNumber() {
+            if (counter.innerText > 0) {
+                counter.style.color = "green";
+            } else if (counter.innerText < 0) {
+                counter.style.color = "red";
+            } else {
+                counter.style.color = "black";
+            }
+        }
     });
 });
+
+counter.addEventListener("")
