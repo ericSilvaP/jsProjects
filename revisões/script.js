ba8beb2042dbab5007d6e58a50dfc453bed05012
Eric Silva <ericsilvap13@gmail.com>
2025-02-15 16:21:18 -0300

    estilos e começo script

---
+++ revisões/script.js
@@ -0,0 +1,14 @@
+const buttons = document.querySelectorAll(".btt");
+
+buttons.forEach(element => {
+    element.addEventListener("click", (e) => {
+        const styles = e.currentTarget.classList;
+        if (styles.contains("back")) {
+            //
+        } else if (styles.contains("back")) {
+            //
+        } else if (styles.contains("back")) {
+            //
+        }
+    })
+});
